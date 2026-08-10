f3b9b5463f4fb4eb190dcf2d319516072765f5f5
Sachin Phapale <phapalesachin107@gmail.com>
2026-08-10 09:43:34 +0530

    Time: 11 ms (14.87%), Space: 44 MB (14.44%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,17 +1,28 @@
-class Solution {
+public class Solution {
     public int maxNumberOfBalloons(String text) {
-        int[] freq = new int[26];
-
+        // Create a frequency map for the input string
+        Map<Character, Integer> freqMap = new HashMap<>();
         for (char c : text.toCharArray()) {
-            freq[c - 'a']++;
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
         }
 
-        return Math.min(
-                Math.min(freq['b' - 'a'], freq['a' - 'a']),
-                Math.min(
-                        Math.min(freq['l' - 'a'] / 2, freq['o' - 'a'] / 2),
-                        freq['n' - 'a']
-                )
-        );
+        // Store the required frequencies of each character in 'balloon'
+        String balloon = "balloon";
+
+        // Calculate the max number of "balloon" words
+        int maxBalloons = Integer.MAX_VALUE;
+        Map<Character, Integer> balloonFreq = new HashMap<>();
+        for (char c : balloon.toCharArray()) {
+            balloonFreq.put(c, balloonFreq.getOrDefault(c, 0) + 1);
+        }
+
+        // Calculate the max possible number of "balloon" we can form
+        for (Map.Entry<Character, Integer> e : balloonFreq.entrySet()) {
+            char key = e.getKey();
+            int count = e.getValue();
+            maxBalloons = Math.min(maxBalloons, freqMap.getOrDefault(key, 0) / count);
+        }
+
+        return maxBalloons;
     }
 }
